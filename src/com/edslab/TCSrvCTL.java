0a9ff456dcca0d4bfbb8219309f51cc96a4e849a
lab-sb <51949811+lab-sb@users.noreply.github.com>
2023-10-02 11:28:13 +0200

    Update TCSrvCTL.java

--- src/com/edslab/TCSrvCTL.java
+++ src/com/edslab/TCSrvCTL.java
@@ -1,8 +1,8 @@
 
 /**
- *   *  Copyright (c) 2022-2023
- *     *  Silvio Brandani <mktg.tcapture@gmail.com>. All rights reserved.
- *      */
+    *  Copyright (c) 2022-2023
+    *  Silvio Brandani <mktg.tcapture@gmail.com>. All rights reserved.
+ **/
 
 package com.edslab;
 
